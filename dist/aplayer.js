9062a0c3c82fa9e9b0f3ef81b05100cdebf68555
yzfly <yz.liu.me@gmail.com>
2019-03-05 07:16:07 +0800

    Site updated: 2019-03-05 07:16:02

--- dist/aplayer.js
+++ dist/aplayer.js
@@ -1,7 +1,18 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: true,
+    mini: false,            <!-- 不启用迷你播放模式 -->
+    fixed: false,           <!-- ture时为浮动效果 -->
+    autoplay: false,        <!-- 音频不自动播放 -->
+    theme: '#FADFA3',       <!-- 主题色 -->
+    loop: 'all',            <!-- 音频循环播放, 可选值: 'all', 'one', 'none'-->
+    order: 'random',        <!-- 音频循环顺序, 可选值: 'list', 'random'-->
+    preload: 'auto',        <!-- 预加载，可选值: 'none', 'metadata', 'auto'-->
+    volume: 0.7,            <!-- 默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效-->
+    mutex: true,            <!-- 互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器 -->
+    listFolded: false,      <!-- 列表默认折叠， false表示不折叠-->
+    listMaxHeight: '120px', <!-- 列表最大高度，超出此高度将有滚动条-->
+    lrcType: 0,             <!-- 歌词类型0表示不显示歌词 -->
+    
     audio: [
       {
         name: "青春纪念册",
