c3b95f1a745cd05bfbee00d8c7cd15a6b4e4c171
yzfly <yz.liu.me@gmail.com>
2019-03-04 23:30:24 +0800

    Site updated: 2019-03-04 23:30:23

--- dist/aplayer.js
+++ dist/aplayer.js
@@ -1,13 +1,19 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
       {
         name: "青春纪念册",
         artist: '可米小子',
-        url: 'http://www.ytmp3.cn/down/43623.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/57.jpg',
+        url: '/musics/青春纪念册.mp3',
+        cover: '/musics/pics/青春纪念册.jpg',
+      },
+      {
+        name: "不过是爱情",
+        artist: '刘惜君',
+        url: '/musics/不过是爱情_刘惜君.mp3',
+        cover: '/musics/pics/不过是爱情.jpg',
       },
       {
         name: '听妈妈的话',
